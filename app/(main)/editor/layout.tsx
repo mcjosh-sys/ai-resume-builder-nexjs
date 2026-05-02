306ca435b3f20b5feb0d0ff8437446fac78f86bc
Joshua Olushola <olusholajoshua7@gmail.com>
2026-05-02 23:25:04 +0100

    feat: add site metadata and Open Graph images for improved SEO and social sharing

--- app/(main)/editor/layout.tsx
+++ app/(main)/editor/layout.tsx
@@ -2,7 +2,13 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import { EditorShellHeader } from "@/features/editor/components/editor-shell-header";
 import { EditorSidebar } from "@/features/editor/components/editor-sidebar";
 import EditorProvider from "@/features/editor/providers/editor-provider";
+import { Metadata } from "next";
 import { PropsWithChildren } from "react";
+
+export const metadata: Metadata = {
+  title: "Editor",
+  description: "CV Copilot Editor",
+};
 
 export default function EditorLayout({ children }: PropsWithChildren) {
   return (
